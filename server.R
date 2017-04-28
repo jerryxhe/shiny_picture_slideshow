97be27e5da6dbad021be8a037d417e7df55e0cd0
Jerry He <jerryhe@Jerrys-MacBook.fios-router.home>
2017-04-28 19:58:24 -0400

    more photos

--- server.R
+++ server.R
@@ -1,12 +1,13 @@
 library(png)
+library(shiny)
 
 ptime <- function(t) {
     t = as.numeric(t)
     fraction = t%%1000000000;
     t = trunc(t/1000000000);
-    hour = trunc(t/3600); 
+    hour = trunc(t/3600);
     minute = (t%%3600);
-    second = minute %% 60; 
+    second = minute %% 60;
     minute = trunc(minute/60);
     sprintf("%02d:%02d:%02d.%09d",hour,minute,second,fraction);
 }
@@ -18,7 +19,7 @@ shinyServer(function(input, output, session) {
   output$image1 <- renderImage({
       width  <- session$clientData$output_image1_width
       height <- session$clientData$output_image1_height
-      
+
       list(src = pics[input$n %% length(pics)],
              contentType = "image/png",
              width = width,
